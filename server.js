a71a480dcba9bbf5da3c887d35c6006e690ed20c
hamzawho <hmasood369@gmail.com>
2024-10-21 21:04:34 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path');
 // const { S3Client } = require('@aws-sdk/client-s3');
-const { Upload } = require('@aws-sdk/lib-storage');
+// const { Upload } = require('@aws-sdk/lib-storage');
 const { exec } = require('child_process'); // Import exec for shell commands
 require('dotenv').config();
 
